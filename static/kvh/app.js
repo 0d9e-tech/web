0d9e000080c01ab19728e9e4b32b438fc7027189
Matuush <matus1@pull.cz>
2025-12-08 23:34:41 +0100

    kolizní rotace fixnutá

--- static/kvh/app.js
+++ static/kvh/app.js
@@ -242,7 +242,7 @@ function updatePhysics() {
                 ball2.vy -= impulse * ny;
 
                 // Exchange some spin
-                const spinExchange = impulse * 0.3;
+                const spinExchange = (ball1.r + ball2.r)/2 / 10;
                 ball1.vr += spinExchange;
                 ball2.vr -= spinExchange;
             }
