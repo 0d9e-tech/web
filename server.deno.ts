b433a1acd9b930c877c522da125057f00a2a84c5
Matěj Volf <git@mvolfik.com>
2025-12-08 21:55:12 +0100

    fix lints (priste uz nebudu tak hodnej)

--- server.deno.ts
+++ server.deno.ts
@@ -102,7 +102,7 @@ async function handleEvent(e: RequestEvent): Promise<Response | null> {
       });
     }
 
-    return new Response(videoBytes as any, {
+    return new Response(videoBytes.buffer, {
       headers: {
         "Content-Type": "video/mp4",
         "Access-Control-Allow-Origin": "*"
@@ -127,7 +127,7 @@ async function handleEvent(e: RequestEvent): Promise<Response | null> {
       });
     }
 
-    return new Response(sticekrBytes as any, {
+    return new Response(sticekrBytes.buffer, {
       headers: {
         "Content-Type": "image/webp"
       },
