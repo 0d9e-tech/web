a8f08c347863cade8145f110a26450f4b02c0eff
Matěj Volf <git@mvolfik.com>
2026-01-06 09:25:36 +0100

    ()))((())()()()(()()))()(()(()()()())(())((((((((())(()()

--- src/tg/bot.deno.ts
+++ src/tg/bot.deno.ts
@@ -197,10 +197,10 @@ export async function* handleTgUpdate(data: any) {
   }
 
   if (
-    text.toLowerCase().includes("pad") ||
-    (text.toLowerCase().includes("pád") &&
-      !text.toLowerCase().includes("vypada") &&
-      !text.toLowerCase().includes("vypadá"))
+    (text.toLowerCase().includes("pad") ||
+      text.toLowerCase().includes("pád")) &&
+    !text.toLowerCase().includes("vypada") &&
+    !text.toLowerCase().includes("vypadá")
   ) {
     yield await tgCall(
       {
