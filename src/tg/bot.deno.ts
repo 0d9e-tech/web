978235ccdf9a1a35843880114c552166762a66d4
Matěj Volf <git@mvolfik.com>
2026-01-06 09:49:18 +0100

    splendid

--- src/tg/bot.deno.ts
+++ src/tg/bot.deno.ts
@@ -196,11 +196,11 @@ export async function* handleTgUpdate(data: any) {
     );
   }
 
+  const tits = text.toLowerCase().replaceAll("á", "a");
   if (
-    (text.toLowerCase().includes("pad") ||
-      text.toLowerCase().includes("pád")) &&
-    !text.toLowerCase().includes("vypada") &&
-    !text.toLowerCase().includes("vypadá")
+    tits.includes("pad") &&
+    !tits.includes("vypada") &&
+    !tits.includes("napad")
   ) {
     yield await tgCall(
       {
